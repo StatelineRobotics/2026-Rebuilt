64c01372a5ec50b14240da4867413c8be154e748
Stateline Robotics FRC Team 4655 <59455257+StatelineRobotics@users.noreply.github.com>
2026-02-12 20:30:21 -0600

    some launcher init

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,14 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+/** Add your docs here. */
+public class Constants {
+    public static final int leftFlyweelId = 20;
+    public static final int rightFlywheelId = 21;
+    public static final int hoodId = 22;
+    public static final int turretId = 23;
+
+}
