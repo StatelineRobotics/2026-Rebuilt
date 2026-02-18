f5a385b62174992c8db118d6f4c214d4d2432bd5
= <ianspace3138@gmail.com>
2026-02-17 22:43:51 -0600

    Shot calculator doneish

--- src/main/java/frc/robot/subsystems/Launcher/ShotCalculator.java
+++ src/main/java/frc/robot/subsystems/Launcher/ShotCalculator.java
@@ -4,7 +4,10 @@
 
 package frc.robot.subsystems.Launcher;
 
+import com.ctre.phoenix6.controls.compound.Diff_PositionVoltage_Velocity;
+
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -15,8 +18,9 @@ public class ShotCalculator {
     private static final Translation2d redHubPose = Translation2d.kZero;
     private static final Translation2d blueHubPose = Translation2d.kZero;
     private static Translation2d targetPose = Translation2d.kZero;
+    private static final int NumItterations = 4;
 
-    public record ShootingSolution(double turretAngle, double hoodAngle, double flywheelSpeed) {}
+    public record ShootingSolution(Rotation2d turretAngle, double hoodAngle, double flywheelSpeed) {}
 
     private static ExtrapolatingDoubleTreeMap tofMap = new ExtrapolatingDoubleTreeMap();
 
@@ -53,10 +57,35 @@ public class ShotCalculator {
             targetPose = redHubPose;
         }
 
-        double distance = robotPose.getTranslation().getDistance(targetPose);
 
-        return new ShootingSolution(0.0,0.0,0.0);
-        
+        double distance = robotPose.getTranslation().getDistance(targetPose);
+        Rotation2d turretAngle = targetPose.minus(robotPose.getTranslation()).getAngle();
+
+        return new ShootingSolution(turretAngle, hoodMap.get(distance), flywheelMap.get(distance));
+    }
+
+    public static ShootingSolution getSOTMhubSolution(Pose2d robotPose, Translation2d robotVelocity) {
+        if (DriverStation.getAlliance().orElseGet(() -> Alliance.Blue) == Alliance.Blue) {
+            targetPose = blueHubPose;
+        } else {
+            targetPose = redHubPose;
+        }
+
+        double distance = 0;
+        //Itterate shot projection to hopefully converge on correct shot
+        for (int i = 0; i < NumItterations; i++) {
+            distance = robotPose.getTranslation().getDistance(targetPose);
+            double tof = tofMap.get(distance);
+            targetPose = targetPose.minus(robotVelocity.times(tof)); //Shift goal by predicted change in flight due to robot velocity
+        }
+
+        Rotation2d turretAngle = targetPose.minus(robotPose.getTranslation()).getAngle();
+
+        //TODO: Check if shot is good (there are situations where it diverges or converges too slowly)
+
+        return new ShootingSolution(turretAngle, hoodMap.get(distance), flywheelMap.get(distance));
+
+
     }
 
 }
